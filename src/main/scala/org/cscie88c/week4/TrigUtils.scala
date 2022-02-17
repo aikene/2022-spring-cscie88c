dbde60be49a3568d1d081081c863fa8a99db3dc2
aikene <ikene.aj@gmail.com>
2022-02-16 22:09:19 -0500

    Problem 1

--- src/main/scala/org/cscie88c/week4/TrigUtils.scala
+++ src/main/scala/org/cscie88c/week4/TrigUtils.scala
@@ -1,13 +1,14 @@
 package org.cscie88c.week4
+import math._
 
 object TrigUtils {
 
   // https://www.scala-lang.org/api/2.13.6/scala/math/index.html
   // use the function literal syntax for sin and cos
-  val sinDegrees: Double => Double = ???
-  val cosDegrees: Double => Double = ???
+  val sinDegrees: Double => Double = (x: Double) => sin(x)
+  val cosDegrees: Double => Double = (x: Double) => cos(x)
 
   // use the placeholder syntax for squared
-  val squared: Double => Double = ???
+  val squared: Double => Double = (x: Double) => x*x
   
 }
--- src/test/scala/org/cscie88c/week4/TrigUtilsTest.scala
+++ src/test/scala/org/cscie88c/week4/TrigUtilsTest.scala
@@ -1,17 +1,41 @@
 package org.cscie88c.week4
 
 import org.cscie88c.testutils.{StandardTest}
+import math._
 
 class TrigUtilsTest extends StandardTest {
   
   "TrigUtils" when {
     "calling sin" should {
       "return the correct value for 90" in {
-        // write unit test below
+        val sinValue: Double = TrigUtils.sinDegrees(90)
+        val expectedSinValue: Double = sin(90) 
+        sinValue should equal(expectedSinValue)
       }
-    
+      "return the correct value for 0" in {
+        val sinValue: Double = TrigUtils.sinDegrees(0)
+        val expectedSinValue: Double = sin(0) 
+        sinValue should equal(expectedSinValue)
+      }    
     }
-
-    // write tests for cos and squared below
+    "calling cos" should {
+      "return the correct value for 90" in {
+        val cosValue: Double = TrigUtils.cosDegrees(90)
+        val expectedCosValue: Double = cos(90) 
+        cosValue should equal(expectedCosValue)
+      }
+      "return the correct value for 0" in {
+        val cosValue: Double = TrigUtils.cosDegrees(0)
+        val expectedCosValue: Double = cos(0) 
+        cosValue should equal(expectedCosValue)
+      }       
+    }
+    "calling squared" should {
+      "return the correct value for 100" in {
+        val squaredValue: Double = TrigUtils.squared(100)
+        val expectedSquaredValue: Double = 100*100
+        squaredValue should equal(expectedSquaredValue)
+      }       
+    }
   }
 }
