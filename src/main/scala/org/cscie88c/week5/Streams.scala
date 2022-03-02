630258cb94b38b07a5fcbd83d376602605586c65
aikene <ikene.aj@gmail.com>
2022-03-01 21:26:55 -0500

    week5

--- src/main/scala/org/cscie88c/week5/Streams.scala
+++ src/main/scala/org/cscie88c/week5/Streams.scala
@@ -9,15 +9,17 @@ object Streams {
 
   case class Dog(name: String, age: Int, hasCurrentShots: Boolean)
 
-  val mult5: LazyList[Int] = ???
+  val mult5: LazyList[Int] = LazyList.range(0, 105, 5)
 
-  val randIntStream: LazyList[Int] = ???
+  val randIntStream: LazyList[Int] = LazyList.continually(rnd.nextInt(15))
 
-  val dogs: LazyList[Dog] = ???
+  val dogs: LazyList[Dog] = LazyList.iterate(Dog("dog-"+uuid, randIntStream.head, rnd.nextBoolean()))(
+    d => d.copy(name = "dog-"+uuid, age = rnd.nextInt(randIntStream.head), hasCurrentShots = rnd.nextBoolean() ) 
+  )
 
-  def healthyDogs(dogs: LazyList[Dog]): LazyList[Dog] = ???
+  def healthyDogs(dogs: LazyList[Dog]): LazyList[Dog] = dogs.filter(_.hasCurrentShots == true)
 
-  def averageHealthyAge(allDogs: LazyList[Dog], sampleSize: Int): Double = ???
+  def averageHealthyAge(allDogs: LazyList[Dog], sampleSize: Int): Double = allDogs.filter(_.hasCurrentShots == true).take(sampleSize).map(_.age).sum / sampleSize
 
 
 }
--- src/test/scala/org/cscie88c/week5/FunctionUtilsTest.scala
+++ src/test/scala/org/cscie88c/week5/FunctionUtilsTest.scala
@@ -9,13 +9,31 @@ class FunctionUtilsTest extends StandardTest {
     // Problem 1 unit tests
     "calling colorToCode" should {
       "return the correct value for white" in {
-        // write unit tests here
+        val whiteRGB = FunctionUtils.colorToCode("White")
+        whiteRGB should be ((255,255,255))
+      }
+      "return the correct value for lime" in {
+        val whiteRGB = FunctionUtils.colorToCode("lime")
+        whiteRGB should be ((0,255,0))
       }
     }
 
     "calling fizzBuzzString" should {
-      "return the correct value" in {
-        // write unit tests here
+      "return the correct value for 15" in {
+        val fizzBuzzString = FunctionUtils.fizzBuzzString(15)
+        fizzBuzzString should be("FizzBuzz")
+      }
+      "return the correct value for 3" in {
+        val fizzBuzzString = FunctionUtils.fizzBuzzString(3)
+        fizzBuzzString should be("Fizz")
+      }
+      "return the correct value for 5" in {
+        val fizzBuzzString = FunctionUtils.fizzBuzzString(5)
+        fizzBuzzString should be("Buzz")
+      }
+      "return the correct value for 17" in {
+        val fizzBuzzString = FunctionUtils.fizzBuzzString(17)
+        fizzBuzzString should be("17")
       }
     }
 
@@ -26,8 +44,45 @@ class FunctionUtilsTest extends StandardTest {
     }
     
     // Problem 2 unit tests
+    "calling isDefinedAt for tanDegrees" should {
+      "return false for 90" in {
+        val expectedBool = FunctionUtils.tanDegrees.isDefinedAt(90)
+        expectedBool should be(false)
+      }
+      "return false for -90" in {
+        val expectedBool = FunctionUtils.tanDegrees.isDefinedAt(-90)
+        expectedBool should be(false)
+      }
+    }
+    "calling high transactions" should {
+      "return correct sum for list of transactions" in {
+        val cs1 = CustomerTransaction("string","date", 1)
+        val cs2 = CustomerTransaction("string","date", 101)
+        val cs3 = CustomerTransaction("string","date", 200)
+
+        val expectedAmount = FunctionUtils.totalHighValueTransactions(List(cs1, cs2, cs3))
+        expectedAmount should be (301)
+      }
+    }
 
     // Problem 3 unit tests
+    "calling flip2" should {
+      "flip the values of a binary function" in {
+        val expectedPower = FunctionUtils.flip2(math.pow)(5,2)
+        expectedPower should be (32.0)
+      }
+    }
+
+    "calling sample list should only return 5 values" should {
+      "with a list of strings" in {
+        val los = List("I","am","a","list","of...","strings")
+        FunctionUtils.sampleList(los) should be (los.take(5))
+      }
+      "with a list of integers" in {
+        val loi = List(1,2,3,4,5,6,7,8)
+        FunctionUtils.sampleList(loi) should be (loi.take(5))
+      }
+    }
 
     // Bonus unit tests
   }
